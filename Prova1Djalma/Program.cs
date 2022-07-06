ff13edffa03b0d42d7009c3c9bc7908d34764297
Rita Franca Faria <105826740+ritaffranca2003@users.noreply.github.com>
2022-07-06 11:35:02 -0300

    Update Program.cs
    Questão 1 : Prova 2° Semestre em Lógica de Programação 
    Prof : Djalma

--- Prova1Djalma/Program.cs
+++ Prova1Djalma/Program.cs
@@ -9,7 +9,20 @@ namespace Prova1Djalma
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
+           
+            ////NOME: Rita de Cassia Franca Faria       RA: 6322054
+            
+            ////1 - Faça um programa que receba o nome do aluno, nota de trabalho, nota da prova 1 e nota da prova 2.Calcule a média 
+            ////final(média simples) e apresente o conceito obtido seguindo a seguinte formula:
+            ////O conceito da será calculado da seguinte forma:
+            
+            ////Se a nota final for maior ou igual a 8.5, então o conceito é A.
+            ////Se a nota final estiver entre 7.0 e 8.4, então o conceito é B.
+            ////Se a nota final estiver entre 6.0 e 6.9, então o conceito é C.
+            ////Se a nota final estiver entre 0.1 e 5.9, então o conceito é D.
+            ////Se a nota final for igual a 0.0, então o conceito é E.
+            
             double n2, n3, n4, media;
             string nome;
 
